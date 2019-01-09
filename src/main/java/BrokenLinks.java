4e24f43bf7472f69427ae71e2536631cd8a33510
Lokesh Reddy <lokesh.reddy@globallogic.com>
2019-01-09 17:12:26 +0530

    just simple commit

--- src/main/java/BrokenLinks.java
+++ src/main/java/BrokenLinks.java
@@ -1,9 +1,14 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
 
@@ -20,7 +25,7 @@ public class BrokenLinks {
         System.setProperty("webdriver.gecko.driver", "C:\\Automate3.0\\SeleniumLearning\\geckodriver.exe");
 
         driver = new FirefoxDriver();
-        //driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(homepage);
 
@@ -28,9 +33,48 @@ public class BrokenLinks {
 
         Iterator<WebElement> it = links.iterator();
 
-        driver.get(homepage);
+        while (it.hasNext()) {
+
+            url = it.next().getAttribute("href");
+
+            System.out.println(url);
+
+            if (url == null || url.isEmpty()) {
+
+                System.out.println("URL is miss configured or is empty()");
+                continue;
+            }
+
+            if (!url.startsWith(homepage)) {
+                System.out.println("url belongs to another page - skipping it");
+                continue;
+            }
+            try {
+                huc = (HttpURLConnection) (new URL(url).openConnection());
+
+                huc.setRequestMethod("HEAD");
+
+                huc.connect();
+
+                resposecode = huc.getResponseCode();
+
+                if (resposecode >= 400) {
+                    System.out.println(url + " is a broken link");
+                } else {
+                    System.out.println(url + " is a valid link");
+                }
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
 
 
+        driver.close();
 
     }
 }
